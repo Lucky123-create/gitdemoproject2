0d72f50ab7256cfd91736eefc2395992eb59277a
Lucky123-create <gouripattapu.mamatha@gmail.com>
2022-05-30 14:17:25 +0530

    first commit issue1

--- src/com/cts/Main.java
+++ src/com/cts/Main.java
@@ -1,9 +1,14 @@
 package com.cts;
 
+import com.service.SomeService;
+
 public class Main {
 	public static void main(String args[]){
 
 		System.out.println("Hello world");
+
+		SomeService ss=new SomeService();
+		System.out.println(ss.findLength("Gowtham"));
 
 	}
 }
---
+++ src/com/service/SomeService.java
@@ -0,0 +1,11 @@
+package com.service;
+
+public class SomeService {
+
+
+	public int findLength(String name) {
+
+		return name.length();
+
+	}
+}
